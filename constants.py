91d13a66bb06d4d07e2b8181fb68701c3d61b67e
petcebch <peter.cebula@spirago.ch>
2023-10-08 22:27:11 +0200

    adjust constnts.py

--- constants.py
+++ constants.py
@@ -88,8 +88,8 @@ EMBEDDING_MODEL_NAME = "hkunlp/instructor-xl" # Uses 5 GB of VRAM (Most Accurate
 ####    32b        130 GB       65 GB       32.5 GB - 35 GB    16.25 GB - 19 GB
 ####    65b        260.8 GB     130.4 GB    65.2 GB - 67 GB    32.6 GB -  - 35 GB
 
-MODEL_ID = "TheBloke/Llama-2-7B-Chat-GGML"
-MODEL_BASENAME = "llama-2-7b-chat.ggmlv3.q4_0.bin"
+# MODEL_ID = "TheBloke/Llama-2-7B-Chat-GGML"
+# MODEL_BASENAME = "llama-2-7b-chat.ggmlv3.q4_0.bin"
 
 ####
 #### (FOR GGUF MODELS)
@@ -142,8 +142,8 @@ MODEL_BASENAME = "llama-2-7b-chat.ggmlv3.q4_0.bin"
 ### 13b GPTQ Models for 24GB GPUs (*** With best embedding model: hkunlp/instructor-xl ***)
 # MODEL_ID = "TheBloke/Wizard-Vicuna-13B-Uncensored-GPTQ"
 # MODEL_BASENAME = "model.safetensors"
-# MODEL_ID = "TheBloke/vicuna-13B-v1.5-GPTQ"
-# MODEL_BASENAME = "model.safetensors"
+MODEL_ID = "TheBloke/vicuna-13B-v1.5-GPTQ"
+MODEL_BASENAME = "model.safetensors"
 # MODEL_ID = "TheBloke/Nous-Hermes-13B-GPTQ"
 # MODEL_BASENAME = "nous-hermes-13b-GPTQ-4bit-128g.no-act.order"
 # MODEL_ID = "TheBloke/WizardLM-13B-V1.2-GPTQ"
